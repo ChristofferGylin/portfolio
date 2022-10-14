efe7ae71fff1987160bc40ab1d310adf0a1bd483
Christoffer <christoffer@christoffergylin.com>
2022-10-14 09:37:13 +0200

    Updated portfolio info texts

--- infoTexts/grooveboxInfo.js
+++ infoTexts/grooveboxInfo.js
@@ -1,4 +1,7 @@
 export const grooveboxInfo = {
     title: 'Web Groovebox',
-    text: 'Boom Boom Tish Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illo adipisci architecto dolorem sit reprehenderit autem, consequuntur hic unde doloremque incidunt deserunt necessitatibus odit, pariatur aspernatur ad eaque maxime impedit numquam, velit voluptatem! Voluptas maxime quasi maiores cumque obcaecati voluptatem nihil aspernatur? Fuga incidunt provident doloribus, necessitatibus temporibus aliquam, fugit modi quis repellendus et ex quod ipsum quo, sunt commodi tenetur sint numquam! Aperiam excepturi perferendis corrupti! Nostrum, maiores odio! Molestias iusto harum aspernatur perferendis fugit adipisci vitae veritatis deserunt saepe, voluptatem tempore maxime nostrum temporibus officiis eos distinctio quam libero, quibusdam sunt, nihil amet non veniam. Voluptatibus, iusto unde?'
+    text: 'Proof of Concept för en webbaserad applikation för musikproduktion, en så kallad DAW, Digital Audio Workstation som jag byggde med hjälp av Tone.js, ett bibliotek för att arbeta med ljud på ett musikaliskt sätt i JavaScript.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'Tone.js'],
+    links: [{ title: 'Live Länk', link: 'https://christoffergylin.com/groovebox' },
+    { title: 'GitHub', link: 'https://www.example.com' }]
 }
--- infoTexts/tinyGrandInfo.js
+++ infoTexts/tinyGrandInfo.js
@@ -1,4 +1,7 @@
 export const tinyGrandInfo = {
     title: 'Tiny Grand Prix',
-    text: 'Wroom Wroom Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illo adipisci architecto dolorem sit reprehenderit autem, consequuntur hic unde doloremque incidunt deserunt necessitatibus odit, pariatur aspernatur ad eaque maxime impedit numquam, velit voluptatem! Voluptas maxime quasi maiores cumque obcaecati voluptatem nihil aspernatur? Fuga incidunt provident doloribus, necessitatibus temporibus aliquam, fugit modi quis repellendus et ex quod ipsum quo, sunt commodi tenetur sint numquam! Aperiam excepturi perferendis corrupti! Nostrum, maiores odio! Molestias iusto harum aspernatur perferendis fugit adipisci vitae veritatis deserunt saepe, voluptatem tempore maxime nostrum temporibus officiis eos distinctio quam libero, quibusdam sunt, nihil amet non veniam. Voluptatibus, iusto unde?'
+    text: 'Ett racingspel i top-down retro stil jag byggt med Phaser 3, en spelmotor för JavaScript, komplett med split screen multiplayer (och buggar), dags att utmana kompisarna på en match!',
+    tech: ['HTML', 'CSS', 'JavaScript', 'Tone.js', 'Phaser 3'],
+    links: [{ title: 'Live Länk', link: 'https://tinygrandprix.herokuapp.com/' },
+    { title: 'GitHub', link: 'https://www.example.com' }]
 }
