6533faa1ca6bec4d88badc65e832c7c82e88e546
Christoffer <christoffer@christoffergylin.com>
2022-10-16 20:21:38 +0200

    Added alert for contact form and styled form text

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+document.getElementById('menuIcon').addEventListener('click', () => {
+
+    document.getElementById('navMobile').classList.toggle('menu-hidden');
+
+});
+
+document.getElementById('formSubmit').addEventListener('click', () => {
+    alert('Det här formuläret har ingen funktionalitet än, skicka ett mail till christoffer@christoffergylin.com istället!')
+});
+
+
+
