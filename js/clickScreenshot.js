de346f1c74ae95d697d43f4fcd2b7e7a50661f06
Christoffer <christoffer@christoffergylin.com>
2022-10-19 15:11:38 +0200

    Trying to fix SVG iPhone Safari fuckery

--- js/clickScreenshot.js
+++ js/clickScreenshot.js
@@ -3,7 +3,7 @@ import { backToPopup } from "./backToPopup.js";
 export const clickScreenshot = (path) => {
     console.log(path);
 
-    const closeIcon = `<div class="popup-close-container" id="closeIconSvgScreenshot"><div class="popup-close-icon-screenshot"><svg class="close-icon-svg" id="closeIconScreenshotSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 90">
+    const closeIcon = `<div class="popup-close-container" id="closeIconSvgScreenshot"><div class="popup-close-icon-screenshot"><svg class="close-icon-svg" id="closeIconScreenshotSvg" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 90">
     <line class="close-icon-line" x1="11.609" y1="6.109" x2="89.391" y2="83.891" />
     <line class="close-icon-line" x1="89.391" y1="6.109" x2="11.609" y2="83.891" />
 </svg></div></div>`
