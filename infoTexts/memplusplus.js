afc0ef2bd8e7c762a7d3c66d3afbd4b2909b2eb5
Christoffer <christoffer@christoffergylin.com>
2023-02-27 17:08:08 +0100

    Added new screenshots for mem++

--- infoTexts/memplusplus.js
+++ infoTexts/memplusplus.js
@@ -5,6 +5,9 @@ export const memplusplusInfo = {
     links: [{ title: 'Live Länk', link: 'https://christoffergylin.github.io/memplusplus/' },
     { title: 'GitHub', link: 'https://github.com/ChristofferGylin/memplusplus' }],
     screenshots: [
-        { thumb: './img/memplusplus-screenshot.JPG', large: './img/memplusplus-screenshot.JPG' }
+        { thumb: './img/memplusplus-screenshot1Small.png', large: './img/memplusplus-screenshot1.png' },
+        { thumb: './img/memplusplus-screenshot2Small.png', large: './img/memplusplus-screenshot2.png' },
+        { thumb: './img/memplusplus-screenshot3Small.png', large: './img/memplusplus-screenshot3.png' },
+
     ]
 }
